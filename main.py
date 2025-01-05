96b40f63aa139501b9a5fce5439ea4b14ebd9661
carnasci <cen.master24@gmail.com>
2025-01-05 17:16:03 -0600

    Day 21 - cleaned up snake head/body collision using slicing method, Day 21 finished

--- main.py
+++ main.py
@@ -49,10 +49,14 @@ while game_is_on:
         scoreboard.game_over()
 
     #Detect snake head collision with snake body
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    # for segment in snake.segments:
+    #     if segment == snake.head:
+    #         pass
+    #     elif snake.head.distance(segment) < 10:
+
+    #Detect snake head and body collision using slicing
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
 
