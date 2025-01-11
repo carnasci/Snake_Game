936fcd18ca64e481e6e400b9140c6b685c4a915b
carnasci <cen.master24@gmail.com>
2025-01-11 15:42:32 -0600

    Day 21 - scoreboard has highscore functionality, and snake resets instead of game over

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ while game_is_on:
 
     #Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     #Detect snake head collision with snake body
     # for segment in snake.segments:
@@ -57,8 +57,8 @@ while game_is_on:
     #Detect snake head and body collision using slicing
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 
